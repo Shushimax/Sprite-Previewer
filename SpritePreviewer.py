e0c39dcba5aec4e910eb521c3c40d656641898da
Shushimax <136651112+Shushimax@users.noreply.github.com>
2026-04-17 01:12:40 -0600

    Update SpritePreviewer.py
    Finished program with text for fps display

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -28,16 +28,19 @@ class Interface(QMainWindow):
         self.animation_slider.setRange(1,100)
         self.animation_slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
 
-        self.next_button = QPushButton("Stop")
+        self.next_button = QPushButton("Start")
 
         self.animation_slider.valueChanged.connect(self.frame_rate_change)
         self.next_button.clicked.connect(self.stopstart)
-        self.isplaying = True
+        self.isplaying = False
 
         self.timer = QTimer()
-        self.framerate = 1
+        self.framerate = self.animation_slider.value()
         self.timer.start(1000 // self.framerate)
         self.timer.timeout.connect(self.next_image)
+
+        self.fpsdisplay = QLabel("Frames Per Second:")
+        self.fpsdisplay_variable = QLabel(str(self.framerate))
 
 
         self.setupUI()
@@ -46,11 +49,27 @@ class Interface(QMainWindow):
         # Central Widget
         frame = QFrame()
 
+        #menu bar
+        menubar = self.menuBar()
+        file_menu = QMenu("&File",self)
+        menubar.addMenu(file_menu)
+        pause = QAction('Pause',self)
+        pause.triggered.connect(self.stop)
+        Exit = QAction('Exit',self)
+        Exit.triggered.connect(self.close)
+
+        file_menu.addAction(pause)
+        file_menu.addAction(Exit)
+
         # Layouts
         main_layout = QVBoxLayout()
         central_layout = QHBoxLayout()
         central_layout.addWidget(self.image_label, alignment=Qt.AlignmentFlag.AlignCenter)
         central_layout.addWidget(self.animation_slider)
+
+        Fps_layout = QHBoxLayout()
+        Fps_layout.addWidget(self.fpsdisplay)
+        Fps_layout.addWidget(self.fpsdisplay_variable)
 
 
         buttons_layout = QVBoxLayout()
@@ -58,6 +77,7 @@ class Interface(QMainWindow):
 
 
         main_layout.addLayout(central_layout)
+        main_layout.addLayout(Fps_layout)
         main_layout.addLayout(buttons_layout)
 
 
@@ -67,25 +87,31 @@ class Interface(QMainWindow):
 
 
     def next_image(self):
-        self.current_pic = (self.current_pic + 1) % self.num_frames
-        self.image_label.setPixmap(self.sprites[self.current_pic])
+        if self.isplaying:
+            self.current_pic = (self.current_pic + 1) % self.num_frames
+            self.image_label.setPixmap(self.sprites[self.current_pic])
 
     def frame_rate_change(self):
         if self.isplaying:
             self.framerate = self.animation_slider.value()
             self.timer.start(1000 // self.framerate)
-            self.timer.timeout.connect(self.next_image)
-            print(self.animation_slider.value())
+        self.fpsdisplay_variable.setText(str(self.animation_slider.value()))
+        print(self.animation_slider.value())
 
     def stopstart(self):
         if self.isplaying:
             self.next_button.setText("Start")
             self.isplaying = False
-            self.timer.stop()
         else:
             self.next_button.setText("Stop")
             self.isplaying = True
+            self.frame_rate_change()
             self.timer.start(1000 // self.framerate)
+
+    def stop(self):
+        self.next_button.setText("Start")
+        self.isplaying = False
+        self.timer.stop()
 
 
 
